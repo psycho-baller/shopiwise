53a4472f579614e4dfedea49063062d0c29aa15d
Rami Maalouf <rami.maalouf@ibm.com>
2024-02-19 17:06:52 -0700

    migrated the backend to go and only kept the basics of it

---
+++ backend/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+type RequestBody struct {
+	ProductTitle string `json:"productTitle"`
+	UserInfo     string `json:"userInfo"`
+}
+func intentions(w http.ResponseWriter, r *http.Request) {
+	    // Check if the request method is POST
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+    }
+	// Parse the request body
+	var requestBody RequestBody
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	userInfo := requestBody.UserInfo
+	productTitle := requestBody.ProductTitle
+	formatResponse := `The output should be formatted as a JSON instance that conforms to the JSON schema below.\n\nAs an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}\nthe object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.\n\nHere is the output schema:\n~~~\n{"properties": {"intentions": {"title": "Intentions", "description": "List of potential unrecognized intentions", "type": "array", "items": {"$ref": "#/definitions/Intention"}}}, "required": ["intentions"], "definitions": {"Intention": {"title": "Intention", "type": "object", "properties": {"title": {"title": "Title", "description": "Title of the potential unrecognized intention", "type": "string"}, "description": {"title": "Description", "description": "The description of the potential unrecognized intention", "type": "string"}}, "required": ["title", "description"]}}}\n~~~'}`
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model:     openai.GPT3Dot5Turbo,
+			// MaxTokens: 5,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: fmt.Sprintf("%s. I found this amazon product named \"%s\" Could you warn me of the potential unrecognized intentions that might be motivating me to buy this item? I wanna ensure that I'm not going to regret buying this item in a few weeks from now.\n %s", userInfo, productTitle, formatResponse),
+				},
+			},
+		},
+	)
+	if err != nil {
+		fmt.Printf("Completion error: %v\n", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+  w.Write([]byte(resp.Choices[0].Message.Content))
+}
+
+func logger(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+				log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+				f(w, r)
+		}
+}
+func main() {
+    http.HandleFunc("/intentions", logger(intentions))
+
+    http.ListenAndServe(":80", nil)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module shopiwise
+
+go 1.22.0
+
+require github.com/sashabaranov/go-openai v1.19.4
