204f6a23c1fe94693a11c2a583d74f35e428217a
Rami Maalouf <rami.maalouf@ibm.com>
2024-02-24 03:18:00 +0400

    lint

--- backend/main.go
+++ backend/main.go
@@ -16,18 +16,18 @@ type RequestBody struct {
 	UserInfo     string `json:"userInfo"`
 }
 func intentions(w http.ResponseWriter, r *http.Request) {
-	    // Check if the request method is POST
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-    }
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	return
+	}
 	// Parse the request body
 	var requestBody RequestBody
 	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
-
+	
 	userInfo := requestBody.UserInfo
 	productTitle := requestBody.ProductTitle
 	formatResponse := `The output should be formatted as a JSON instance that conforms to the JSON schema below.\n\nAs an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}\nthe object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.\n\nHere is the output schema:\n~~~\n{"properties": {"intentions": {"title": "Intentions", "description": "List of potential unrecognized intentions", "type": "array", "items": {"$ref": "#/definitions/Intention"}}}, "required": ["intentions"], "definitions": {"Intention": {"title": "Intention", "type": "object", "properties": {"title": {"title": "Title", "description": "Title of the potential unrecognized intention", "type": "string"}, "description": {"title": "Description", "description": "The description of the potential unrecognized intention", "type": "string"}}, "required": ["title", "description"]}}}\n~~~'}`
@@ -50,17 +50,17 @@ func intentions(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-  w.Write([]byte(resp.Choices[0].Message.Content))
+	w.Write([]byte(resp.Choices[0].Message.Content))
 }
 
 func logger(f http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-				log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-				f(w, r)
-		}
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		f(w, r)
+	}
 }
 func main() {
-    http.HandleFunc("/intentions", logger(intentions))
-
-    http.ListenAndServe(":80", nil)
+	http.HandleFunc("/intentions", logger(intentions))
+	
+	http.ListenAndServe(":80", nil)
 }
