5b8f7dfbdc29675992623a66018f5d26b5b3a3ba
Rami Maalouf <rami.maalouf@ibm.com>
2024-02-24 03:18:32 +0400

    Update llm.ts

--- browser-extension/lib/llm.ts
+++ browser-extension/lib/llm.ts
@@ -1,5 +1,5 @@
 export async function fetchIntentions(productTitle: string, userInfo: string) {
-	const res = await fetch('http://fastapi.localhost/intentions', {
+	const res = await fetch('http://localhost:80/intentions', {
 		method: 'POST',
 		body: JSON.stringify({ productTitle, userInfo }),
 		headers: {
